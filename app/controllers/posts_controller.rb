5b9ad841904f72ffc44ff1630f88086b5cd6ce04
pndesu <tranvanphucnguyen1@gmail.com>
2023-02-06 16:36:15 +1100

    Add views and methods in user, post controllers

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,25 @@
 class PostsController < ApplicationController
+    before_action :authenticate_user!, except: [:index]
+
     def index
 
     end
 
     def new
-
+        @post = Post.new
     end
 
     def create
-        
+        @post = Post.new(post_params)
+        if @post.save
+            redirect_to root
+        else
+            render :new, status: :unprocessable_entity
+        end
+    end
+
+    private
+    def post_params
+        params.require(:post).permit(:title, :content)
     end
 end
