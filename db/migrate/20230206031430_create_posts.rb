b37cbe2e375d3437d4ce76b547d9dedc7eae0cb1
pndesu <tranvanphucnguyen1@gmail.com>
2023-02-06 14:46:12 +1100

    Add devise to user model

---
+++ db/migrate/20230206031430_create_posts.rb
@@ -0,0 +1,12 @@
+class CreatePosts < ActiveRecord::Migration[7.0]
+  def change
+    create_table :posts do |t|
+      t.string :author
+      t.text :content
+      t.text :title
+      t.references :user, index: true, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
